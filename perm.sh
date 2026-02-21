29b62e0ab89583e052a1d0bb6ee3fc273bdc7bfc
Aissyne Amine <amine.aissyne06@icloud.com>
2026-02-21 15:38:00 +0100

    merge

---
+++ perm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+chmod -R 777 bin/
+chmod -R 777 JAVA/legotools/C/input/
+chmod -R 777 JAVA/legotools/C/output/
+echo "les permissions ont bien été mises à jour."
+ls -l bin/
+ls -l JAVA/legotools/C/input/
+ls -l JAVA/legotools/C/output/
